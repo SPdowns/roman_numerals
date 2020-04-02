d294265ba9621d388111e47db93a1980ba6cf3db
Sean Downs <spdowns@protonmail.com>
2020-04-02 14:20:02 -0700

    notes an idea. going to students to bounce it off them

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,19 @@
 // Back End
 
 var romanNums = ["I", "V", "X", "L", "C", "D", "M"]
-var romanOne = $("input#numValue").val();
-var inputNumber = romanOne
+var numberNumbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
 
-var romanFinish = function(romanOne) {
-  
-  var outputNumber = inputNumber
-  return outputNumber
-  
-}
-console.log(romanFinish)
+// if romanOne can be seperate into single digits and identify numerical tens tenths and hundreths
+// place, and run 89 like 80 = 50, 10, 10, 10 and 9 = 10 - 1
+for each
+
 // Front End
 
 $(document).ready(function(){
   $("form#roman-form").submit(function(event){
     event.preventDefault();
     
-    
+    var romanOne = $("input#numValue").val();
     
     $("#result").html(result);
   });
