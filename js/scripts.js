56f2624059d96205c95ea5b3f01bd24dceb2daa3
Sean Downs <spdowns@protonmail.com>
2020-04-02 11:37:34 -0700

    adds array and console logging

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,17 @@
 
 var romanNums = ["I", "V", "X", "L", "C", "D", "M"]
 
-
 // Front End
 
 $(document).ready(function(){
 
-  $(form).submit(function(event){
+  $("form#form-group").submit(function(event){
     event.preventDefault();
     
+    var numValue = ("input#numValue").val();
+    console.log (numValue)
     var result = form(submit);
   });
-    $(".output").html();
+    $("#result").html(result);
+    console.log(result)
 });
