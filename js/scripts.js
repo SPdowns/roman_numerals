fbc1033d95d8f8360ccf704d19c853626b474e06
Sean Downs <spdowns@protonmail.com>
2020-04-02 15:23:33 -0700

    tried looking into another method

--- js/scripts.js
+++ js/scripts.js
@@ -3,17 +3,18 @@
 var romanNums = ["I", "V", "X", "L", "C", "D", "M"]
 var numberNumbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
 
-var romanFinish = function(romanOne) {
-  var inputNumber = romanOne.split();
-}
-console.log(romanOne)
+var romanOne = $("input#numValue").val();
+// var romanFinish = function(romanOne) {
+//   var inputNumber = romanOne.split();
+// }
+// console.log(romanOne)
 // Front End
 
 $(document).ready(function(){
   $("form#roman-form").submit(function(event){
     event.preventDefault();
     
-    var romanOne = $("input#numValue").val();
+    
     
     $("#result").html(romanOne);
   });
