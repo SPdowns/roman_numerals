c8c94b7fd32a150319ddb4c13f7c6bf32f606023
Sean Downs <spdowns@protonmail.com>
2020-04-02 11:27:17 -0700

    adds a var

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 // Back End
 
-
+var romanNums = ["I", "V", "X", "L", "C", "D", "M"]
 
 
 // Front End
@@ -9,6 +9,8 @@ $(document).ready(function(){
 
   $(form).submit(function(event){
     event.preventDefault();
+    
+    var result = form(submit);
   });
     $(".output").html();
 });
