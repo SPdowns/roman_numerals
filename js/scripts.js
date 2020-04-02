3dc562c86c5e89f092ae79f5a0f20608a18f73bd
Sean Downs <spdowns@protonmail.com>
2020-04-02 15:23:33 -0700

    attempts shown. going to need to review

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,12 @@
 // Back End
 
-var romanNums = ["I", "V", "X", "L", "C", "D", "M"]
-var numberNumbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
-
-var romanFinish = function(romanOne) {
-  var inputNumber = romanOne.split();
-}
-console.log(romanOne)
 // Front End
 
 $(document).ready(function(){
   $("form#roman-form").submit(function(event){
     event.preventDefault();
-    
-    var romanOne = $("input#numValue").val();
-    
-    $("#result").html(romanOne);
   });
-   
-});
 
+  var romanOne = $("input#numValue").val();
+  $("#result").html(result);
+});
